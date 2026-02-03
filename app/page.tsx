8b9a0687f2d1ba9ab7e62313b9fe5ff002d769a4
rikvart <vartiainenriku@gmail.com>
2026-02-03 16:29:46 +0200

    work on testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { createClient } from '@/utils/supabase/server'
 import { cookies } from 'next/headers'
 
+
 export default async function Page() {
   const cookieStore: any = await cookies()
   const supabase = createClient(cookieStore)
@@ -8,15 +9,33 @@ export default async function Page() {
   const { data: todos } = await supabase.from('todos').select()
 
   return (
-    <div>
-    <ul>
-      {todos?.map((todo) => (
-        <li>{todo}</li>
-      ))}
-    </ul>
-    <section>
-      text
-    </section>
+    // 'flex-col' stacks them vertically; 'gap-4' adds spacing
+    <div className="flex flex-col gap-8 p-4">
+      
+      {/* The Form as a Flex Container */}
+      <form className="flex flex-wrap flex-col gap-2">
+        <input name="Climb" placeholder="Climb" className="flex-1 border border-black p-2" />
+        <input name="here" placeholder="here" className="flex-1 border border-black p-2" />
+        <input name="here" placeholder="here" className="flex-1 border border-black p-2" />
+        <input name="here" placeholder="here" className="flex-1 border border-black p-2" />
+        <input name="here" placeholder="here" className="flex-1 border border-black p-2" />
+        <input name="here" placeholder="here" className="flex-1 border border-black p-2" />
+        
+        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
+          Submit
+        </button>
+      </form>
+
+      <ul>
+        {todos?.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+
+      <section className="mt-4">
+        text
+      </section>
     </div>
   )
 }
+  
